740314ee39a7846535a626c1b8529200f141299d
jsupratman13 <supratmanjoshua@gmail.com>
2016-11-09 14:00:22 +0900

    fix bug

--- test/hello.c
+++ test/hello.c
@@ -9,14 +9,14 @@ MODULE_VERSION("1.0");
 
 static char *name = "world";
 module_param(name, charp, S_IRUGO);
-MODULE_PARAM_DESC(name, "string displayed in log");
+//MODULE_PARAM_DESC(name, "string displayed in log");
 
-static int __init helloworld_init(){
+static int __init helloworld_init(void){
 	printk(KERN_INFO "Hello is a %s !\n", name);
 	return 0;
 }
 
-static void __exit helloworld_exit(){
+static void __exit helloworld_exit(void){
 	printk(KERN_INFO "Goodbye is a %s !\n", name);
 }
 
